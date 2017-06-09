ae7be7f92834f0affef45642a2ec380d7d62d870
Jan Pazdziora <jpazdziora@redhat.com>
2017-06-09 13:06:44 +0200

    Redirect to LOGIN_REDIRECT_URL after logon.

--- activity/urls.py
+++ activity/urls.py
@@ -1,10 +1,10 @@
 from django.conf.urls import url
 
 from . import views
-from django.contrib.auth.views import login, logout
+from django.contrib.auth.views import logout
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^login/$', login, name='login', kwargs={'template_name': 'activity/login.html'}),
+    url(r'^login/$', views.login, name='login'),
     url(r'^logout/$', logout, name='logout', kwargs={'next_page': '/'}),
 ]
