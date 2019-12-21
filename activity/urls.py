72dbef203524886e84e5f1cc9af8561d8e8baad2
Jan Pazdziora <jpazdziora@redhat.com>
2019-12-21 12:14:23 +0100

    Redirect to LOGIN_REDIRECT_URL after external logon when found logged in already.

--- activity/urls.py
+++ activity/urls.py
@@ -1,5 +1,5 @@
 
-# Copyright 2016 Jan Pazdziora
+# Copyright 2016--2019 Jan Pazdziora
 #
 # Licensed under the Apache License, Version 2.0 (the "License").
 
@@ -10,6 +10,6 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('login', auth_views.LoginView.as_view(template_name='activity/login.html'), name='login'),
+    path('login', views.login, name='login'),
     path('logout', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
 ]
--- activity/views.py
+++ activity/views.py
@@ -1,10 +1,15 @@
 
-# Copyright 2016 Jan Pazdziora
+# Copyright 2016--2019 Jan Pazdziora
 #
 # Licensed under the Apache License, Version 2.0 (the "License").
 
-from django.shortcuts import render
+from django.conf import settings
+from django.shortcuts import render, resolve_url
+from django.contrib.auth import REDIRECT_FIELD_NAME
 from django.contrib.auth.models import User
+from django.contrib.auth.views import LoginView
+from django.http import HttpResponseRedirect
+from django.utils.http import is_safe_url
 
 def index(request):
     activity_list = User.objects.order_by('-last_login')[:20]
@@ -17,3 +22,12 @@ def index(request):
         'activity_list': activity_list,
     }
     return render(request, 'activity/index.html', context)
+
+def login(request, template_name='activity/login.html', redirect_field_name=REDIRECT_FIELD_NAME):
+    if hasattr(request, 'user') and request.user.is_authenticated:
+        redirect_to = request.POST.get(redirect_field_name,
+            request.GET.get(redirect_field_name, ''))
+        if not is_safe_url(url=redirect_to, allowed_hosts=request.get_host()):
+            redirect_to = resolve_url(settings.LOGIN_REDIRECT_URL)
+        return HttpResponseRedirect(redirect_to)
+    return LoginView.as_view(template_name = template_name, redirect_field_name = redirect_field_name)(request)
